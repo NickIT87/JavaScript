a0da2feb3a64ed8edc17ae6197ae320dda913876
Nickolazz1 <elanir358@gmail.com>
2020-08-15 17:30:17 +0300

    some changes

--- ThreeJS/TestProj/static/js/main.js
+++ ThreeJS/TestProj/static/js/main.js
@@ -12,18 +12,48 @@ function init() {
     renderer.setSize( window.innerWidth, window.innerHeight );
     container.appendChild( renderer.domElement );
 
+    
     // main logic  
     scene.add(lines());
+    // light helper
+    var light = new THREE.DirectionalLight( 0xFFFFFF );
+    var helper = new THREE.DirectionalLightHelper( light, 155 );
+    scene.add( helper );
+    // myplane not visible
+    scene.add(myplane());
+    
 
     // start app
     renderer.render(scene, camera);
 }
 
 
+function myplane() {
+
+    var geometry = new THREE.PlaneGeometry(Window.innerWidth / Window.innerHeight);
+    geometry.applyMatrix4(new THREE.Matrix4().makeRotationX(-Math.PI/2));
+    var material = new THREE.MeshBasicMaterial({color: 0xFFFFFF});
+    var plane = new THREE.Mesh(geometry, material);
+
+    console.log("my plane")
+    return plane;
+
+}
+
+
 function lines() {
-    var line_geometry = new THREE.Geometry();
+    var line_geometry = new THREE.Geometry();   // производная фигура
+    line_geometry.vertices.push(new THREE.Vector3(0,0,0));  // x, y, z - создаем вектор
+    line_geometry.vertices.push(new THREE.Vector3(100,0,0));
+    line_geometry.vertices.push(new THREE.Vector3(100,100,0));
+    line_geometry.vertices.push(new THREE.Vector3(0,100,0));
     line_geometry.vertices.push(new THREE.Vector3(0,0,0));
-    line_geometry.vertices.push(new THREE.Vector3(0,150,0));
-    var line = new THREE.Line(line_geometry);
+
+
+    var material_line = new THREE.LineBasicMaterial({color: 0x5BC2D5, linewidth: 1, opacity: 1});
+
+    var line = new THREE.Line(line_geometry, material_line); // реальный объект
+
+
     return line;
 }
