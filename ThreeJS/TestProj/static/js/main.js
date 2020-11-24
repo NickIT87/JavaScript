39ec2f28debff4cd6c652e7128adaa16f1da02b8
Nickolazz1 <elanir358@gmail.com>
2020-11-24 13:53:03 +0200

    base architecture

--- ThreeJS/TestProj/static/js/main.js
+++ ThreeJS/TestProj/static/js/main.js
@@ -1,26 +1,5 @@
 var scene, camera, renderer;
 
-
-// test example function
-function lines() {
-    var line_geometry = new THREE.Geometry();   // производная фигура
-    line_geometry.vertices.push(new THREE.Vector3(0,0,0));  // x, y, z - создаем вектор
-    line_geometry.vertices.push(new THREE.Vector3(100,0,0));
-    line_geometry.vertices.push(new THREE.Vector3(100,100,0));
-    line_geometry.vertices.push(new THREE.Vector3(0,100,0));
-    line_geometry.vertices.push(new THREE.Vector3(0,0,0));
-
-
-    var material_line = new THREE.LineBasicMaterial({color: 0x5BC2D5, linewidth: 1, opacity: 1});
-
-    var line = new THREE.Line(line_geometry, material_line); // реальный объект
-
-    console.log("lines");
-
-    return line;
-}
-
-
 function init() {
     var container = document.createElement('div');
     container.className = "app";
@@ -38,19 +17,30 @@ function init() {
     var helper = new THREE.DirectionalLightHelper( light, 155 );
     scene.add( helper );
     
-
     // main logic  
     //scene.add(lines());      // test function
-    scene.add(custom_triangle());
-
-
+    main();
     // start app
     renderer.render(scene, camera);
 }
 
+// test example function
+function lines() {
+    var line_geometry = new THREE.Geometry();   // производная фигура
+    line_geometry.vertices.push(new THREE.Vector3(0,0,0));  // x, y, z - создаем вектор
+    line_geometry.vertices.push(new THREE.Vector3(100,0,0));
+    line_geometry.vertices.push(new THREE.Vector3(100,100,0));
+    line_geometry.vertices.push(new THREE.Vector3(0,100,0));
+    line_geometry.vertices.push(new THREE.Vector3(0,0,0));
+
+    var material_line = new THREE.LineBasicMaterial({color: 0x5BC2D5, linewidth: 1, opacity: 1});
+    var line = new THREE.Line(line_geometry, material_line); // реальный объект
+    console.log("lines");
+
+    return line;
+}
 
 function custom_triangle() {
-
     var line_geometry = new THREE.Geometry();   // производная фигура
     line_geometry.vertices.push(new THREE.Vector3(100,100,0));  // x, y, z - создаем вектор
     line_geometry.vertices.push(new THREE.Vector3(0,100,0));
@@ -58,13 +48,13 @@ function custom_triangle() {
     line_geometry.vertices.push(new THREE.Vector3(0,0,0));
     line_geometry.vertices.push(new THREE.Vector3(-1000,0,0));
 
-
     var material_line = new THREE.LineBasicMaterial({color: 0x00FF00, linewidth: 1, opacity: 1});
-
     var line = new THREE.Line(line_geometry, material_line); // реальный объект
-
     console.log("custom triangle");
     
     return line;
 }
 
+function main() {
+    scene.add(custom_triangle());
+}
