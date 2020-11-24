a58d0f7e0146dfbdd3f0d8412f16c30918f13084
Nickolazz1 <elanir358@gmail.com>
2020-11-24 12:47:22 +0200

    three js review

--- ThreeJS/TestProj/static/js/main.js
+++ ThreeJS/TestProj/static/js/main.js
@@ -1,46 +1,7 @@
 var scene, camera, renderer;
 
-function init() {
-    var container = document.createElement('div');
-    container.className = "app";
-    document.body.appendChild(container)
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.y = 150;
-    camera.position.z = 600;
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    container.appendChild( renderer.domElement );
 
-    
-    // main logic  
-    scene.add(lines());
-    // light helper
-    var light = new THREE.DirectionalLight( 0xFFFFFF );
-    var helper = new THREE.DirectionalLightHelper( light, 155 );
-    scene.add( helper );
-    // myplane not visible
-    scene.add(myplane());
-    
-
-    // start app
-    renderer.render(scene, camera);
-}
-
-
-function myplane() {
-
-    var geometry = new THREE.PlaneGeometry(Window.innerWidth / Window.innerHeight);
-    geometry.applyMatrix4(new THREE.Matrix4().makeRotationX(-Math.PI/2));
-    var material = new THREE.MeshBasicMaterial({color: 0xFFFFFF});
-    var plane = new THREE.Mesh(geometry, material);
-
-    console.log("my plane")
-    return plane;
-
-}
-
-
+// test example function
 function lines() {
     var line_geometry = new THREE.Geometry();   // производная фигура
     line_geometry.vertices.push(new THREE.Vector3(0,0,0));  // x, y, z - создаем вектор
@@ -54,6 +15,56 @@ function lines() {
 
     var line = new THREE.Line(line_geometry, material_line); // реальный объект
 
+    console.log("lines");
 
     return line;
 }
+
+
+function init() {
+    var container = document.createElement('div');
+    container.className = "app";
+    document.body.appendChild(container)
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.y = 150;
+    camera.position.z = 600;
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    container.appendChild( renderer.domElement );
+
+    // light helper
+    var light = new THREE.DirectionalLight( 0xFFFFFF );
+    var helper = new THREE.DirectionalLightHelper( light, 155 );
+    scene.add( helper );
+    
+
+    // main logic  
+    //scene.add(lines());      // test function
+    scene.add(custom_triangle());
+
+
+    // start app
+    renderer.render(scene, camera);
+}
+
+
+function custom_triangle() {
+
+    var line_geometry = new THREE.Geometry();   // производная фигура
+    line_geometry.vertices.push(new THREE.Vector3(100,100,0));  // x, y, z - создаем вектор
+    line_geometry.vertices.push(new THREE.Vector3(0,100,0));
+    line_geometry.vertices.push(new THREE.Vector3(100,0,0));
+    line_geometry.vertices.push(new THREE.Vector3(0,0,0));
+    line_geometry.vertices.push(new THREE.Vector3(-1000,0,0));
+
+
+    var material_line = new THREE.LineBasicMaterial({color: 0x00FF00, linewidth: 1, opacity: 1});
+
+    var line = new THREE.Line(line_geometry, material_line); // реальный объект
+
+    console.log("custom triangle");
+    
+    return line;
+}
+
