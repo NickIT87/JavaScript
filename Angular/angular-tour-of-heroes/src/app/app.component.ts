572492f29a29501177ec9639b0be7046b46875b9
Nickolazz1 <elanir358@gmail.com>
2021-01-14 22:42:14 +0200

    tour of heroes

---
+++ Angular/angular-tour-of-heroes/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Tour of heroes!'
+  title2: number = sum(2, 3)
+}
+
+function sum(a: number, b: number): number {
+  return a + b
+}
