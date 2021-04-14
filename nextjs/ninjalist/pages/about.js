2de52f9896b5517f8e83c0384366055f6d3a878d
salamanka <elanir358@gmail.com>
2021-04-14 14:44:22 +0300

    5 lessons completed, start from 6

---
+++ nextjs/ninjalist/pages/about.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>about</h1>
+            <p>lorem ipsum dolor sit amet consectetur ... </p>
+        </div>
+    );
+}
+ 
+export default About;
--- nextjs/ninjalist/pages/index.js
+++ nextjs/ninjalist/pages/index.js
@@ -1,10 +1,17 @@
 import Head from 'next/head'
+import Footer from '../comps/Footer'
+import Navbar from '../comps/Navbar'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div>
       <h1>Homepage</h1>
+      <p>Some text content</p>
+      <Link href="/ninjas">
+        <a>See Ninja Listing</a>
+      </Link>
     </div>    
   )
 }
