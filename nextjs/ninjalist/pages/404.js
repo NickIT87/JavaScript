91febc905db4ea9e1c79d0fe9680b8bd71388e0e
salamanka <elanir358@gmail.com>
2021-04-15 18:05:45 +0300

    less 8 compleete

--- nextjs/ninjalist/pages/404.js
+++ nextjs/ninjalist/pages/404.js
@@ -1,7 +1,19 @@
 import Link from 'next/link'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { route } from 'next/dist/next-server/server/router';
 
 // sfc snippet, stateless functional component
 const NotFound = () => {
+    const router = useRouter();
+    
+    useEffect(() => {
+        //console.log('use effect run')
+        setTimeout(() => {
+            router.push('/')
+        }, 3000)
+    }, [])
+
     return ( 
         <div className="not-found">
             <h1>Ooops...</h1>
