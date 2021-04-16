4b269bbc51d63d19ffe600b907ba02eb492d334b
salamanka <elanir358@gmail.com>
2021-04-16 15:16:54 +0300

    less 8 compleete

--- nextjs/ninjalist/pages/about.js
+++ nextjs/ninjalist/pages/about.js
@@ -1,9 +1,17 @@
+import Head from 'next/head'
+
 const About = () => {
-    return ( 
+    return (
+        <>
+        <Head>
+            <title>Ninja List | About</title>
+            <meta name="keywords" content="ninjas"/>
+        </Head>
         <div>
             <h1>about</h1>
             <p>lorem ipsum dolor sit amet consectetur ... </p>
         </div>
+        </>
     );
 }
  
--- nextjs/ninjalist/pages/index.js
+++ nextjs/ninjalist/pages/index.js
@@ -6,12 +6,18 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
+    <>
+    <Head>
+      <title>Ninja List | Home</title>
+      <meta name="keywords" content="ninjas"/>
+    </Head>
     <div>
       <h1 className={styles.title}>Homepage</h1>
       <p className={styles.text}>Some text content lorem ipsum dolor sit amet adipicesing elit nestie ...</p>
       <Link href="/ninjas">
         <a className={styles.btn}>See Ninja Listing</a>
       </Link>
-    </div>    
+    </div>
+    </>
   )
 }
