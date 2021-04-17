1d44f4c875e88e9760a1da4501296e50edb2da29
salamanka <elanir358@gmail.com>
2021-04-17 14:54:37 +0300

    TypeError: data.map is not a function

--- nextjs/ninjalist/pages/ninjas/[id].js
+++ nextjs/ninjalist/pages/ninjas/[id].js
@@ -1,7 +1,34 @@
-const Details = () => {
+export const getStaticPaths = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = res.json()
+    // TypeError: data.map is not a function
+    const paths = data.map(ninja => {
+        return {
+            params: { id: ninja.id.toString() }
+        }
+    })
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export const getStaticProps = async (context) => {
+    const id = context.params.id
+    const res = await fetch('https://jsonplaceholder.typicode.com/users/'+id)
+    const data = await res.json()
+    return {
+        props: { ninja: data }
+    }
+}
+
+const Details = ({ ninja }) => {
     return ( 
         <div>
-            <h1>Details</h1>
+            <h1>{ ninja.name }</h1>
+            <p>{ ninja.email }</p>
+            <p>{ ninja.website }</p>
+            <p>{ ninja.address.city }</p>
         </div>
      );
 }
