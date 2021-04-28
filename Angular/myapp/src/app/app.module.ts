6fa50256acd8f05c6a25f058425314577e0e7eeb
Nickolazz1 <elanir358@gmail.com>
2021-04-28 19:56:23 +0300

    angular tutorial

---
+++ Angular/myapp/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StorageService } from './storage.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    StorageService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
