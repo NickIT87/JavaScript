936b0b0a14d27c55793d3dd182b39cc7c733bd62
Nickolazz1 <elanir358@gmail.com>
2021-07-01 15:04:25 +0300

    express review

---
+++ Express/cmscart/app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const path = require('path')
+const mongoose = require('mongoose')
+const config = require('./config/database')
+
+// Connect to db
+mongoose.connect(config.database);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('Connected to MongoDB')
+});
+
+// Init app
+const app = express()
+
+// View engine setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+// Set public folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.send('Working')
+})
+
+// Start the server
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log(`Server started on port: ${PORT}...`)
+})
