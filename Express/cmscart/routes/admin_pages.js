de2c65189f7966bf57abffb4e88762ab183b39c5
Nickolazz1 <elanir358@gmail.com>
2021-07-02 19:27:03 +0300

    admin area add page tab

--- Express/cmscart/routes/admin_pages.js
+++ Express/cmscart/routes/admin_pages.js
@@ -1,9 +1,22 @@
 const express = require('express')
-const router = express.Router()
+var router = express.Router()
 
 router.get('/', (req, res) => {
     res.send("admin area")
 })
+
+router.get('/add-page', (req, res) => {
+    let title = ""
+    let slug = ""
+    let content = ""
+    
+    res.render('admin/add_page', {
+        title: title,
+        slug: slug,
+        content: content
+    })
+})
+
 
 // Exports
 module.exports = router
