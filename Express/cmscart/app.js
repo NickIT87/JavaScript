d753944896fa5013ff38f53a96e7ea30c0a3e674
Nickolazz1 <elanir358@gmail.com>
2021-07-06 17:53:13 +0300

    product model

--- Express/cmscart/app.js
+++ Express/cmscart/app.js
@@ -4,15 +4,20 @@ const mongoose = require('mongoose')
 const config = require('./config/database')
 const session = require('express-session')
 const expressValidator = require('express-validator')
+const fileUpload = require('express-fileupload')
 
 
 // Connect to db
-mongoose.connect(config.database, {useNewUrlParser: true, useUnifiedTopology: true});
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+mongoose.connect(config.database, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    useFindAndModify: false
+})
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', function() {
     console.log('Connected to MongoDB')
-});
+})
 
 // Init app
 const app = express()
@@ -27,9 +32,12 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Set global errors variable
 app.locals.errors = null
 
+// Express fileUpload middleware
+app.use(fileUpload())
+
 // Body parser deprecated - express middleware
 app.use(express.urlencoded({extended: false}))
-app.use(express.json());
+app.use(express.json())
 
 // Express session middleware
 app.use(session({
@@ -60,20 +68,22 @@ app.use(expressValidator({
 }))
 
 // Express Messages middleware
-app.use(require('connect-flash')());
+app.use(require('connect-flash')())
 app.use(function (req, res, next) {
-  res.locals.messages = require('express-messages')(req, res);
+  res.locals.messages = require('express-messages')(req, res)
   next();
-});
+})
 
 
 // Set routes
 const pages = require('./routes/pages')
 const adminPages = require('./routes/admin_pages')
 const adminCategories = require('./routes/admin_categories')
+const adminProducts = require('./routes/admin_products')
 
 app.use('/admin/pages', adminPages)
 app.use('/admin/categories', adminCategories)
+app.use('/admin/products', adminProducts)
 app.use('/', pages)
 
 
---
+++ Express/cmscart/routes/admin_products.js
@@ -0,0 +1,201 @@
+const express = require('express')
+const router = express.Router()
+const mkdrp = require('mkdirp')
+const fs = require('fs-extra')
+const resizeImg = require('resize-img')
+
+// GET Product model
+const Product = require('../models/product')
+
+// // GET pages index
+// router.get('/', (req, res) => {
+//     Page.find({}).sort({sorting: 1}).exec((err, pages) => {
+//         res.render('admin/pages', {
+//             pages: pages
+//         })
+//     })
+// })
+
+// // GET add page
+// router.get('/add-page', (req, res) => {
+//     let title = ""
+//     let slug = ""
+//     let content = ""
+    
+//     res.render('admin/add_page', {
+//         title: title,
+//         slug: slug,
+//         content: content
+//     })
+// })
+
+// // POST add page and save to DB
+// router.post('/add-page', (req, res) => {
+    
+//     req.checkBody('title', 'Title must have a value.').notEmpty();
+//     req.checkBody('content', 'Content must have a value.').notEmpty();
+    
+//     let title = req.body.title
+//     let slug = req.body.slug.replace(/\s+/g, '-').toLowerCase();
+//     if (slug == "") {
+//         slug = title.replace(/\s+/g, '-').toLowerCase()
+//     } 
+//     let content = req.body.content
+//     let errors = req.validationErrors();
+
+//     if (errors) {
+//         console.log('errors:', errors)
+//         res.render('admin/add_page', {
+//             errors: errors,
+//             title: title,
+//             slug: slug,
+//             content: content
+//         })
+//     } else {
+//         //console.log('.../add-page req.post success')
+//         Page.findOne({slug: slug}, (err, page) => {
+//             if (err) {
+//                 console.log("ERROR router/post", err)
+//             }
+//             if (page) {
+//                 req.flash('danger', 'Page slug exists, choose another.')
+//                 res.render('admin/add_page', {
+//                     title: title,
+//                     slug: slug,
+//                     content: content
+//                 })
+//             } else {
+//                 let page = new Page({
+//                     title: title,
+//                     slug: slug,
+//                     content: content,
+//                     sorting: 100
+//                 })
+//                 page.save((err) => {
+//                     if (err) 
+//                         return console.log(err)
+//                     req.flash('success', 'Page added!')
+//                     res.redirect('/admin/pages')
+//                 })
+//             }
+//         })
+//     }
+// })
+
+
+// // POST reorder pages
+// router.post('/reorder-pages', (req, res) => {
+//     //console.log(req.body)
+//     let ids = req.body['id[]']
+//     let count = 0;
+
+//     for (let i = 0; i < ids.length; i++) {
+//         let id = ids[i]
+//         count++
+//         // node async for reorder pages
+//         (function(count) {
+//             Page.findById(id, (err, page) => {
+//                 if (err) {
+//                     console.log("ERROR POST reorder pages: ", err)
+//                 }
+//                 page.sorting = count
+//                 page.save((err) => {
+//                     if (err) {
+//                         return console.log(err)
+//                     }
+//                 })
+//             })
+//         })(count);
+//     }
+// })
+
+
+// // GET edit page
+// router.get('/edit-page/:id', (req, res) => {
+    
+//     Page.findById(req.params.id, (err, page) => {
+//         if (err)
+//             return console.log(err)
+
+//         res.render('admin/edit_page', {
+//             title: page.title,
+//             slug: page.slug,
+//             content: page.content,
+//             id: page._id
+//         })
+//     })
+// })
+
+
+// // POST edit page
+// router.post('/edit-page/:id', (req, res) => {
+    
+//     req.checkBody('title', 'Title must have a value.').notEmpty();
+//     req.checkBody('content', 'Content must have a value.').notEmpty();
+    
+//     let title = req.body.title
+//     let slug = req.body.slug.replace(/\s+/g, '-').toLowerCase();
+//     if (slug == "") {
+//         slug = title.replace(/\s+/g, '-').toLowerCase()
+//     } 
+//     let content = req.body.content
+//     let id = req.params.id
+//     let errors = req.validationErrors();
+
+//     if (errors) {
+//         console.log('errors:', errors)
+//         res.render('admin/edit_page', {
+//             errors: errors,
+//             title: title,
+//             slug: slug,
+//             content: content,
+//             id: id
+//         })
+//     } else {
+//         //console.log('.../edit-page req.post success')
+//         Page.findOne({slug: slug, _id: {'$ne': id}}, (err, page) => {
+//             if (err) {
+//                 console.log("ERROR router/post", err)
+//             }
+//             if (page) {
+//                 req.flash('danger', 'Page slug exists, choose another.')
+//                 res.render('admin/edit_page', {
+//                     title: title,
+//                     slug: slug,
+//                     content: content,
+//                     id: id
+//                 })
+//             } else {
+//                 Page.findById(id, (err, page) => {
+//                     if (err)
+//                         return console.log(err)
+//                     page.title = title
+//                     page.slug = slug
+//                     page.content = content
+
+//                     page.save((err) => {
+//                         if (err) 
+//                             return console.log(err)
+//                         req.flash('success', 'Page edited successful!')
+//                         res.redirect('/admin/pages/edit-page/' + id)
+//                     })
+//                 })
+//             }
+//         })
+//     }
+// })
+
+
+// // GET delete page
+// router.get('/delete-page/:id', (req, res) => {
+//     Page.findByIdAndRemove(req.params.id, (err) => {
+//         if (err)
+//             return console.log(err)
+//         req.flash('success', 'Page deleted!')
+//         res.redirect('/admin/pages/')
+//     })
+// })
+
+
+// Exports
+module.exports = router
