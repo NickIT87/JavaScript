4568bddc579346d2b397e77d26bee14644079696
Nickolazz1 <elanir358@gmail.com>
2021-07-09 15:51:50 +0300

    Get delete image

--- Express/cmscart/routes/admin_products.js
+++ Express/cmscart/routes/admin_products.js
@@ -262,6 +262,28 @@ router.post('/product-gallery/:id', (req, res) => {
 })
 
 
+// GET delete image
+router.get('/delete-image/:image', (req, res) => {
+    let originalImage = 'public/product_images/' + req.query.id + '/gallery/' + req.params.image
+    let thumbImage = 'public/product_images/' + req.query.id + '/gallery/thumbs/' + req.params.image
+
+    fs.remove(originalImage, (err) => {
+        if (err) {
+            console.log(err)
+        } else {
+            fs.remove(thumbImage, (err) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    req.flash('success', 'Image deleted.')
+                    res.redirect('/admin/products/edit-product/' + req.query.id)
+                }
+            })
+        }
+    })
+})
+
+
 // // GET delete page
 // router.get('/delete-page/:id', (req, res) => {
 //     Page.findByIdAndRemove(req.params.id, (err) => {
