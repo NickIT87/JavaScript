65b4da119a8540552fbcd56c5c846842f97f2cb0
Nickolazz1 <elanir358@gmail.com>
2021-07-10 16:06:24 +0300

    display page content

--- Express/cmscart/routes/pages.js
+++ Express/cmscart/routes/pages.js
@@ -1,14 +1,41 @@
 const express = require('express')
 const router = express.Router()
+// Get Page model
+const Page = require('../models/page')
 
+
+// GET /
 router.get('/', (req, res) => {
-    res.render('index', {
-        title: 'Home'
+    Page.findOne({slug: 'home'}, (err, page) => {
+        if (err) {
+            console.log(err)
+        }
+        
+        res.render('index', {
+            title: page.title,
+            content: page.content
+        })
     })
 })
 
-router.get('/test', (req, res) => {
-    res.send("test page")
+
+// GET a page
+router.get('/:slug', (req, res) => {
+    let slug = req.params.slug
+
+    Page.findOne({slug: slug}, (err, page) => {
+        if (err) {
+            console.log(err)
+        }
+        if (!page) {
+            res.redirect('/')
+        } else {
+            res.render('index', {
+                title: page.title,
+                content: page.content
+            })
+        }
+    })
 })
 
 
