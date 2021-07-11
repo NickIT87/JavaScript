3a6e161a5143d432b11e9e67ec8c6c5bcf3c5a59
Nickolazz1 <elanir358@gmail.com>
2021-07-11 11:11:55 +0300

    add product to cart

--- Express/cmscart/routes/cart.js
+++ Express/cmscart/routes/cart.js
@@ -4,18 +4,46 @@ const router = express.Router()
 const Product = require('../models/product')
 
 
-// GET /
-// router.get('/', (req, res) => {
-//     Page.findOne({slug: 'home'}, (err, page) => {
-//         if (err) {
-//             console.log(err)
-//         }
-//         res.render('index', {
-//             title: page.title,
-//             content: page.content
-//         })
-//     })
-// })
+// GET add product to cart
+router.get('/add/:product', (req, res) => {
+    let slug = req.params.product
+
+    Product.findOne({slug: slug}, (err, p) => {
+        if (err) {
+            console.log(err)
+        }
+        if (typeof req.session.cart == "undefined") {
+            req.session.cart = []
+            req.session.cart.push({
+                title: slug,
+                qty: 1,
+                price: parseFloat(p.price).toFixed(2),
+                image: '/product_images/' + p.id + '/' + p.image
+            })
+        } else {
+            let cart = req.session.cart
+            let newItem = true
+            for (let i = 0; i < cart.length; i++) {
+                if (cart[i].title == slug) {
+                    cart[i].qty++
+                    newItem = false
+                    break
+                }
+            }
+            if (newItem) {
+                cart.push({
+                    title: slug,
+                    qty: 1,
+                    price: parseFloat(p.price).toFixed(2),
+                    image: '/product_images/' + p.id + '/' + p.image
+                })
+            }
+        }
+        //console.log(req.session.cart)
+        req.flash('success', 'Product added!')
+        res.redirect('back')
+    })
+})
 
 
 // Exports
