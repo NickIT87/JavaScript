83fd08fc291495dc1056b339223306dff758f37b
Nickolazz1 <elanir358@gmail.com>
2021-07-12 14:22:20 +0300

    cart update

--- Express/cmscart/routes/cart.js
+++ Express/cmscart/routes/cart.js
@@ -48,10 +48,48 @@ router.get('/add/:product', (req, res) => {
 
 // GET checkout view page
 router.get('/checkout', (req, res) => {
-    res.render('checkout', {
-        title: 'Checkout',
-        cart: req.session.cart
-    })
+    if (req.session.cart && req.session.cart.length == 0) {
+        delete req.session.cart
+        res.redirect('/cart/checkout')
+    } else {
+        res.render('checkout', {
+            title: 'Checkout',
+            cart: req.session.cart
+        })
+    }
+})
+
+
+// GET update product
+router.get('/update/:product', (req, res) => {
+    let slug = req.params.product
+    let cart = req.session.cart
+    let action = req.query.action
+    for (let i = 0; i < cart.length; i++) {
+        if (cart[i].title == slug) {
+            switch(action) {
+                case "add":
+                    cart[i].qty++
+                    break
+                case "remove":
+                    cart[i].qty--
+                    if (cart[i].qty < 1) 
+                        cart.splice(i, 1)  
+                    break
+                case "clear":
+                    cart.splice(i, 1)
+                    if (cart.length == 0) 
+                        delete req.session.cart
+                    break
+                default:
+                    console.log('update problem')
+                    break
+            }
+            break
+        }
+    }
+    req.flash('success', 'Cart updated.')
+    res.redirect('/cart/checkout')
 })
 
 
