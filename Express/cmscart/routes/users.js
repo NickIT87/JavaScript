75895eb30e95a285fc2b82be95f523610db18acd
Nickolazz1 <elanir358@gmail.com>
2021-07-13 17:37:57 +0300

    register post incomplete ...

--- Express/cmscart/routes/users.js
+++ Express/cmscart/routes/users.js
@@ -3,11 +3,27 @@ const router = express.Router()
 const passport = require('passport')
 const bcrypt = require('bcryptjs')
 // Get Page model
-const Users = require('../models/user')
+const User = require('../models/user')
 
 
 // GET register
 router.get('/register', (req, res) => {
+    res.render('register', {
+        title: 'Register'
+    })
+})
+
+
+// POST register
+router.get('/register', (req, res) => {
+    let name = req.body.name
+    let email = req.body.email
+    let username = req.body.username
+    let password = req.body.password
+    let password2 = req.body.password2
+
+    // ... 
+    
     res.render('register', {
         title: 'Register'
     })
