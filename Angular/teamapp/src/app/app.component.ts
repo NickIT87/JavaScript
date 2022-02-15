773b927cfcb47c6a27bcbcdcc3fd26ce93ec0dd3
Nickolazz1 <elanir358@gmail.com>
2022-02-15 18:00:08 +0200

    think about angular

--- Angular/teamapp/src/app/app.component.ts
+++ Angular/teamapp/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'teamapp';
+    newMemberName = ""
+    members: string[] = []
+
+    addMember() {
+        this.members.push(this.newMemberName)
+        console.log(this.members)
+    }
 }
