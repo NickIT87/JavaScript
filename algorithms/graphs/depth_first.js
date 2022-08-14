b0ca77c1e4c05d70bc48d41b00bcd5e6a97a76f4
Nickolazz1 <elanir358@gmail.com>
2022-08-14 13:18:46 +0300

    depth first

--- algorithms/graphs/depth_first.js
+++ algorithms/graphs/depth_first.js
@@ -1,11 +1,24 @@
-const depthFirstPrint = (graph, source) => {
-    const stack = [ source ]
+// const depthFirstPrint = (graph, source) => {
+//     const stack = [ source ]
+//     console.log(current)
 
-    while (stack.length > 0) {
-        const current = stack.pop()
-        console.log(current)
+//     while (stack.length > 0) {
+//         const current = stack.pop()
+
+//         for (let neighbor of graph[current]) {
+//             stack.push(neighbor)
+//         }
+//     }
+// }
+
+
+const depthFirstPrint = (graph, source) => {
+    console.log(source)
+    for (let neighbor of graph[source]) {
+        depthFirstPrint(graph, neighbor)
     }
 }
+
 
 const graph = {
     a: ['b', 'c'],
