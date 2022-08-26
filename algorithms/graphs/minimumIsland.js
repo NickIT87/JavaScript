7bf8f09f5b5150f4e6f2ad7ae2c1d7f3f907cb91
Nickolazz1 <elanir358@gmail.com>
2022-08-26 13:44:07 +0300

    minimum island

---
+++ algorithms/graphs/minimumIsland.js
@@ -0,0 +1,48 @@
+const minimumIsland = (grid) => {
+    const visited = new Set()
+    
+    let minSize = Infinity
+    for (let r=0; r<grid.length; r+=1) {
+        for (let c=0; c<grid[0].length; c+=1) {
+            const size = exploreSize(grid, r, c, visited)
+            if (size > 0 && size < minSize) {
+                minSize = size
+            }
+        }
+    }
+    return minSize
+}
+
+
+const exploreSize = (grid, r, c, visited) => {
+    const rowInbounds = 0 <= r && r < grid.length
+    const colInbounds = 0 <= c && c < grid[0].length
+    if (!rowInbounds || !colInbounds) return 0
+
+    if (grid[r][c] === 'W') return 0
+
+    const pos = r + ',' + c
+    if (visited.has(pos)) return 0
+    visited.add(pos)
+
+    let size = 1
+    size += exploreSize(grid, r-1, c, visited)
+    size += exploreSize(grid, r+1, c, visited)
+    size += exploreSize(grid, r, c-1, visited)
+    size += exploreSize(grid, r, c+1, visited)
+
+    return size
+}
+
+
+const grid = [
+    ['W', 'L', 'W', 'W', 'W'],
+    ['W', 'L', 'W', 'W', 'W'],
+    ['W', 'W', 'W', 'L', 'W'],
+    ['W', 'W', 'L', 'L', 'W'],
+    ['L', 'W', 'W', 'L', 'L'],
+    ['L', 'L', 'W', 'W', 'W']
+]
+
+
+console.log(minimumIsland(grid)) // -> 2
