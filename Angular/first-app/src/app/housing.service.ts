8b14d45be5dea90f20780421dae262fb6ebbb88b
NickIT87 <elanir358@gmail.com>
2023-06-10 16:42:24 +0300

    completed

--- Angular/first-app/src/app/housing.service.ts
+++ Angular/first-app/src/app/housing.service.ts
@@ -6,117 +6,129 @@ import { HousingLocation } from './housinglocation';
 })
 export class HousingService {
 
+  url = 'http://localhost:3000/locations';
+
   constructor() { }
 
-  housingLocationList: HousingLocation[] = [
-    {
-      id: 0,
-      name: 'Acme Fresh Start Housing',
-      city: 'Chicago',
-      state: 'IL',
-      photo: '/assets/example-house.jpg',
-      availableUnits: 4,
-      wifi: true,
-      laundry: true
-    },
-    {
-      id: 1,
-      name: 'A113 Transitional Housing',
-      city: 'Santa Monica',
-      state: 'CA',
-      photo: '/assets/example-house.jpg',
-      availableUnits: 0,
-      wifi: false,
-      laundry: true
-    },
-    {
-      id: 2,
-      name: 'Warm Beds Housing Support',
-      city: 'Juneau',
-      state: 'AK',
-      photo: '/assets/example-house.jpg',
-      availableUnits: 1,
-      wifi: false,
-      laundry: false
-    },
-    {
-      id: 3,
-      name: 'Homesteady Housing',
-      city: 'Chicago',
-      state: 'IL',
-      photo: '/assets/example-house.jpg',
-      availableUnits: 1,
-      wifi: true,
-      laundry: false
-    },
-    {
-      id: 4,
-      name: 'Happy Homes Group',
-      city: 'Gary',
-      state: 'IN',
-      photo: '/assets/example-house.jpg',
-      availableUnits: 1,
-      wifi: true,
-      laundry: false
-    },
-    {
-      id: 5,
-      name: 'Hopeful Apartment Group',
-      city: 'Oakland',
-      state: 'CA',
-      photo: '/assets/example-house.jpg',
-      availableUnits: 2,
-      wifi: true,
-      laundry: true
-    },
-    {
-      id: 6,
-      name: 'Seriously Safe Towns',
-      city: 'Oakland',
-      state: 'CA',
-      photo: '/assets/example-house.jpg',
-      availableUnits: 5,
-      wifi: true,
-      laundry: true
-    },
-    {
-      id: 7,
-      name: 'Hopeful Housing Solutions',
-      city: 'Oakland',
-      state: 'CA',
-      photo: '/assets/example-house.jpg',
-      availableUnits: 2,
-      wifi: true,
-      laundry: true
-    },
-    {
-      id: 8,
-      name: 'Seriously Safe Towns',
-      city: 'Oakland',
-      state: 'CA',
-      photo: '/assets/example-house.jpg',
-      availableUnits: 10,
-      wifi: false,
-      laundry: false
-    },
-    {
-      id: 9,
-      name: 'Capital Safe Towns',
-      city: 'Portland',
-      state: 'OR',
-      photo: '/assets/example-house.jpg',
-      availableUnits: 6,
-      wifi: true,
-      laundry: true
-    }
-  ];
+  // housingLocationList: HousingLocation[] = [
+  //   {
+  //     id: 0,
+  //     name: 'Acme Fresh Start Housing',
+  //     city: 'Chicago',
+  //     state: 'IL',
+  //     photo: '/assets/example-house.jpg',
+  //     availableUnits: 4,
+  //     wifi: true,
+  //     laundry: true
+  //   },
+  //   {
+  //     id: 1,
+  //     name: 'A113 Transitional Housing',
+  //     city: 'Santa Monica',
+  //     state: 'CA',
+  //     photo: '/assets/example-house.jpg',
+  //     availableUnits: 0,
+  //     wifi: false,
+  //     laundry: true
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'Warm Beds Housing Support',
+  //     city: 'Juneau',
+  //     state: 'AK',
+  //     photo: '/assets/example-house.jpg',
+  //     availableUnits: 1,
+  //     wifi: false,
+  //     laundry: false
+  //   },
+  //   {
+  //     id: 3,
+  //     name: 'Homesteady Housing',
+  //     city: 'Chicago',
+  //     state: 'IL',
+  //     photo: '/assets/example-house.jpg',
+  //     availableUnits: 1,
+  //     wifi: true,
+  //     laundry: false
+  //   },
+  //   {
+  //     id: 4,
+  //     name: 'Happy Homes Group',
+  //     city: 'Gary',
+  //     state: 'IN',
+  //     photo: '/assets/example-house.jpg',
+  //     availableUnits: 1,
+  //     wifi: true,
+  //     laundry: false
+  //   },
+  //   {
+  //     id: 5,
+  //     name: 'Hopeful Apartment Group',
+  //     city: 'Oakland',
+  //     state: 'CA',
+  //     photo: '/assets/example-house.jpg',
+  //     availableUnits: 2,
+  //     wifi: true,
+  //     laundry: true
+  //   },
+  //   {
+  //     id: 6,
+  //     name: 'Seriously Safe Towns',
+  //     city: 'Oakland',
+  //     state: 'CA',
+  //     photo: '/assets/example-house.jpg',
+  //     availableUnits: 5,
+  //     wifi: true,
+  //     laundry: true
+  //   },
+  //   {
+  //     id: 7,
+  //     name: 'Hopeful Housing Solutions',
+  //     city: 'Oakland',
+  //     state: 'CA',
+  //     photo: '/assets/example-house.jpg',
+  //     availableUnits: 2,
+  //     wifi: true,
+  //     laundry: true
+  //   },
+  //   {
+  //     id: 8,
+  //     name: 'Seriously Safe Towns',
+  //     city: 'Oakland',
+  //     state: 'CA',
+  //     photo: '/assets/example-house.jpg',
+  //     availableUnits: 10,
+  //     wifi: false,
+  //     laundry: false
+  //   },
+  //   {
+  //     id: 9,
+  //     name: 'Capital Safe Towns',
+  //     city: 'Portland',
+  //     state: 'OR',
+  //     photo: '/assets/example-house.jpg',
+  //     availableUnits: 6,
+  //     wifi: true,
+  //     laundry: true
+  //   }
+  // ];
 
-  getAllHousingLocations(): HousingLocation[] {
-    return this.housingLocationList;
-  }
+  // getAllHousingLocations(): HousingLocation[] {
+  //   return this.housingLocationList;
+  // }
   
-  getHousingLocationById(id: number): HousingLocation | undefined {
-    return this.housingLocationList.find(housingLocation => housingLocation.id === id);
+  async getAllHousingLocations(): Promise<HousingLocation[]> {
+    const data = await fetch(this.url);
+    return await data.json() ?? [];
+  }
+
+  // getHousingLocationById(id: number): HousingLocation | undefined {
+  //   return this.housingLocationList.find(housingLocation => housingLocation.id === id);
+  // }
+
+  async getHousingLocationById(id: number): Promise<HousingLocation | undefined> {
+    const data = await fetch(`${this.url}/${id}`);
+    return await data.json() ?? {};
   }
 
   submitApplication(firstName: string, lastName: string, email: string) {
